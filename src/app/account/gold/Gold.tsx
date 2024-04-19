9ed17f327ac50c45bd2f2b8fcea28c47e03ddb50
Minh Giang <minhgiang241@gmail.com>
2024-04-19 16:36:21 +0700

    merge duc

--- src/app/account/gold/Gold.tsx
+++ src/app/account/gold/Gold.tsx
@@ -1,7 +1,19 @@
+import { Divider } from "antd";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Gold() {
-  return <div>Gold</div>;
+  const { t } = useTranslation("account");
+  const common = useTranslation();
+  return (
+    <div className="w-full p-5 flex flex-col">
+      <div className="w-full title_semibold_20">{t("gold_manage")}</div>
+      <div className="caption_regular_14">
+        <span>{t("current_gold")}</span>
+      </div>
+      <Divider className="my-5" />
+    </div>
+  );
 }
 
 export default Gold;
