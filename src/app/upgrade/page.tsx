46e6ade7273b9e6938cab371f45712bc5ff67c06
Hai Duc <haiduczxzx@gmail.com>
2024-04-26 11:12:57 +0700

    push

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -6,10 +6,15 @@ import ConfirmModal from "../components/modals/ConfirmModal";
 import { successToast } from "../components/toast/customToast";
 import { useRouter } from "next/navigation";
 import { loadPackage } from "@/services/api_services/upgrade";
+import { useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
+
 
 export default function Upgrage() {
   const [data, setData] = useState<any>();
-  // const router = useRouter();
+  const router = useRouter();
+  const user = useAppSelector((state: RootState) => state.user.user)
+  console.log('user', user);
 
   const load = async () => {
     const res = await loadPackage()
@@ -228,33 +233,97 @@ export default function Upgrage() {
             </div>
           </div> */}
           {data?.map((x: any, key: any) => (
-            <div key={key} className="max-md:col-span-1 md:col-span-2 lg:col-span-1 border boder-m_neutral_200 rounded-xl bg-white p-6 gap-6 relative">
-              <div className="body_bold_16">{x?.name}</div>
-              <div className="flex items-center">
-                <div className="body_semibold_20 pr-1 title_regular_20">{x?.price}</div>
-                <div className='text-m_neutral_500 body_regular_14'>/ {x?.unit === "year" ? "Năm" : x?.unit === "month" ? "Tháng" : x?.unit === "day" ? "Ngày" : ""}</div>
+            x?.type != "Enterprise" ? (
+              ""
+            ) : (
+              <div key={key} className="max-md:col-span-1 md:col-span-2 lg:col-span-1 border boder-m_neutral_200 rounded-xl bg-white p-6 gap-6 relative">
+                <div className="body_bold_16">{x?.name}</div>
+                <div className="flex items-center">
+                  <div className="body_semibold_20 pr-1 title_regular_20">{x?.price}</div>
+                  <div className='text-m_neutral_500 body_regular_14'>{!x?.price ? <div className="h-7" /> : x?.unit === "year" ? "/ Năm" : x?.unit === "month" ? "/ Tháng" : x?.unit === "day" ? "/ Ngày" : <div className="h-7" />}</div>
+                </div>
+                <div className="py-5 w-full  ">
+                  {x?.features?.map((e: any, key: any) => (
+                    <div key={key} className="flex">
+                      <Tick className="min-w-7" />
+                      <div className="body_regular_14 text-m_neutral_900">{e?.text}</div>
+                    </div>
+                  ))}
+                </div>
+                <div className="h-16" />
+                <div className="absolute bottom-0 w-[222px]">
+                  <hr />
+                  {/* <button
+                    onClick={() => {
+                      successToast("Gửi yêu cầu báo giá thành công !");
+                    }}
+                    className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_primary_500 body_semibold_14 text-white my-4"
+                  >
+                    Nhận báo giá
+                  </button> */}
+                  {/* {!x?.price && <button className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_neutral_200 body_semibold_14 text-m_neutral_500 my-4">
+                    Miễn phí
+                  </button>}
+                  {x?.price && <button
+                    onClick={() => {
+                      router.push("/payment");
+                    }}
+                    className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_primary_500 body_semibold_14 text-white my-4"
+                  >
+                    Đăng kí ngay
+                  </button>} */}
+                  {/* {!x?.price ? (
+                    <button
+                      className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_neutral_200 body_semibold_14 text-m_neutral_500 my-4"
+                    >
+                      Miễn phí
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        router.push(
+                          `/payment?type=Package&packageIdId=${x._id ?? ""
+                          }&price=${x?.price ?? 0}&name=${x?.name ?? ""
+                          }`,
+                        );
+                      }}
+                      className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_primary_500 body_semibold_14 text-white my-4"
+                    >
+                      Đăng kí ngay
+                    </button>
+                  )
+                    // (!x?.price && custom_price === true)
+                  } */}
+                  {(x?.custom_price === true) ? (
+                    <button
+                      onClick={() => {
+                        successToast("Gửi yêu cầu báo giá thành công !");
+                      }}
+                      className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_primary_500 body_semibold_14 text-white my-4"
+                    >
+                      Báo giá
+                    </button>
+                  ) : !x?.price ? (
+                    <button
+                      className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_neutral_200 body_semibold_14 text-m_neutral_500 my-4"
+                    >
+                      Miễn phí
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        router.push(
+                          `/payment?type=Package&packageIdId=${x._id ?? ""}&price=${x?.price ?? 0}&name=${x?.name ?? ""}`,
+                        );
+                      }}
+                      className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_primary_500 body_semibold_14 text-white my-4"
+                    >
+                      Đăng kí ngay
+                    </button>
+                  )}
+                </div>
               </div>
-              <div className="py-5 w-full  ">
-                {x?.features?.map((e: any, key: any) => (
-                  <div key={key} className="flex">
-                    <Tick className="min-w-7" />
-                    <div className="body_regular_14 text-m_neutral_900">{e?.text}</div>
-                  </div>
-                ))}
-              </div>
-              <div className="h-16" />
-              <div className="absolute bottom-0 w-[222px]">
-                <hr />
-                <button
-                  onClick={() => {
-                    successToast("Gửi yêu cầu báo giá thành công !");
-                  }}
-                  className="flex justify-center items-center rounded-lg w-full h-[44px] bg-m_primary_500 body_semibold_14 text-white my-4"
-                >
-                  Nhận báo giá
-                </button>
-              </div>
-            </div>
+            )
           ))}
         </div>
       </div>
